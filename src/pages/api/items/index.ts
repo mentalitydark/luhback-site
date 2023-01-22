655d70057a356c41dbf4fbea4c6cd8a13d14e125
MentalityDark <guilherme.oliveira.01@outlook.com>
2023-01-22 00:48:23 -0300

    Create route GET ALL and GET ONE of Items

---
+++ src/pages/api/items/index.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import Exception from '@/classes/Exception'
+import Items from '@/classes/Items'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        switch(req.method) {
+          case 'GET':
+            return methodGet(req, res)
+          default:
+            throw new Exception("Method not supported.", 400)
+        }
+    } catch (error: any) {
+        if(error instanceof Exception)
+            return res.status(error.code).json(error)
+
+        return res.status(500).json({message: error.message})
+    }
+}
+
+function methodGet(req: NextApiRequest, res: NextApiResponse) {
+    return res.status(200).json((new Items()).getAll())
+}
