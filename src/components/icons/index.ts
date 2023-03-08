dbd0090a4e085515b70806f885c7cfda6b5e987b
MentalityDark <guilherme.oliveira.01@outlook.com>
2023-03-08 08:29:00 -0300

    Add icons svg and output from SVGR

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Instagram } from "./Instagram";
+export { default as OnlyFans } from "./OnlyFans";
+export { default as Telegram } from "./Telegram";
+export { default as Twitter } from "./Twitter";
