8413c658f0f0be8321d966d84699e927f2a4a028
MentalityDark <guilherme.oliveira.01@outlook.com>
2023-03-08 08:32:54 -0300

    Change styles

--- src/components/Pages/Links/Link/styles.ts
+++ src/components/Pages/Links/Link/styles.ts
@@ -1,28 +1,28 @@
-import { Box, Typography, styled } from "@mui/material";
-import Image from "next/image";
+import { Link as _Link, styled, Typography } from "@mui/material";
 
-export const Text = styled(Typography)`
-    flex: 1;
-    text-align: center;
-    color: #383838;
-    font-weight: 600
-`
-
-export const Img = styled(Image)`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-`
-
-export const Container = styled(Box)`
+export const Link = styled(_Link)`
+    text-decoration: none;
     display: flex;
     flex-direction: row;
     align-items: center;
-    border: 1px solid #fff;
+    border: 1px solid #fdfcfc;
     padding: 7px 5px;
     margin: 15px 0;
     border-radius: 50px;
     cursor: pointer;
-    background-color: #fff;
+    background-color: #e3a6b0;
     width: clamp(300px, 100%, 500px);
+    font-family: arial, sans-serif;
+    &:hover {
+        filter: brightness(0.9)
+    }
+`
+
+export const Text = styled(Typography)`
+    flex: 1;
+    text-align: center;
+    color: #fdfcfc;
+    font-weight: 600;
+    text-decoration: none;
+    letter-spacing: 2px
 `
