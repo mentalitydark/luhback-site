021282f07ccfe05c119e67617ae0ca68e0cd2e11
MentalityDark <guilherme.oliveira.01@outlook.com>
2023-03-08 08:30:55 -0300

    Change colors; Change Text to Title

--- src/components/Pages/Links/styles.ts
+++ src/components/Pages/Links/styles.ts
@@ -3,11 +3,12 @@ import _Image from "next/image";
 
 export const Image = styled(_Image)`
   border-radius: 50%;
-  border: 3px solid #fff
+  border: 3px solid #c64949;
+  object-fit: cover;
 `
 
 export const Container = styled(Box)`
-    background-color: #383838;
+    background-color: #f8d7d7;
     margin: 0;
     width: 100vw;
     height: 100vh;
@@ -24,9 +25,10 @@ export const Content = styled(Box)`
     width: 50%;
 `
 
-export const Text = styled(Typography)`
-  color: #fff;
+export const Title = styled(Typography)`
+  color: #c64949;
   font-weight: bold;
   font-size: 20px;
   margin-bottom: 20px;
+  text-align: center;
 `
